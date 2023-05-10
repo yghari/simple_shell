b66c23624209fedb396c2d26e74063848595c0c9
yghari <ghariyouness90@gmail.com>
2023-05-10 19:48:51 +0100

    the start of the minishell

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+
+#endif
---
+++ shell.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+int	main(int ac, char **av, char **env)
+{
+  char	*str;
+
+  while (1)
+  {
+    str = readline("$_> ");
+  }
+  return (0);
+}
