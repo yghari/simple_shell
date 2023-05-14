9f3e273f730fd521149aaa10188cc309fd6cf112
yghari <ghariyouness90@gmail.com>
2023-05-14 02:14:31 +0100

    finally

--- shell.c
+++ shell.c
@@ -6,130 +6,166 @@ void    prompt(void)
 
   printf("%s", prompt);
 }
+
+void free_buff(char **buf)
+{
+    int i = 0;
+
+    if (buf == NULL)
+        return;
+
+    while (buf[i])
+    {
+        free(buf[i]);
+        i++;
+    }
+
+    free(buf);
+}
+
 char **parse(char *str)
 {
-  char **args = malloc(2 * sizeof(char *));
-  int a_i = 0;
-  char *to_ken;
-  to_ken = strtok(str, " \t\n");
-  while (to_ken != NULL)
-  {
-    args[a_i] = strdup(to_ken);
-    a_i++;
-    args = realloc(args, (a_i + 1) * sizeof(char *));
-    to_ken = strtok(NULL, " \t\n");
-  }
-  args[a_i] = NULL;
-  return args;
-}
-char  *bring_path(const char *path)
-{
-  int s_path = strlen(path);
-  char **c_pa;
-
-  for (c_pa = environ; *c_pa != NULL ; c_pa++)
-  {
-    if (strncmp(path, *c_pa, s_path) == 0 && *(*c_pa + s_path) == '=')
+    int size = 2;
+    char **args = malloc(size * sizeof(char *));
+    int a_i = 0;
+    char *to_ken;
+    to_ken = strtok(str, " \t\n");
+    while (to_ken != NULL)
     {
-      return (*c_pa + s_path + 1);
+        args[a_i] = strdup(to_ken);
+        a_i++;
+        if (a_i == size) {
+            size *= 2;
+            args = realloc(args, size * sizeof(char *));
+        }
+        to_ken = strtok(NULL, " \t\n");
     }
-  }
-  return (NULL);
+    args[a_i] = NULL;
+    return args;
+}
+
+char *bring_path(const char *path)
+{
+    int path_len = strlen(path);
+    for (char **env = environ; *env != NULL; env++)
+    {
+        if (strncmp(*env, path, path_len) == 0 && *(*env + path_len) == '=') {
+            return (*env + path_len + 1);
+        }
+    }
+    printf("path not found\n");
+  
+    return NULL;
 }
 
 char **pathing(char *str)
 {
-  char **paths = malloc(2 * sizeof(char *));
-  int i = 0;
-  char *path;
-  path = strtok(str, ":");
-  while (path != NULL)
-  {
-    paths[i] = strdup(path);
-    i++;
-    paths = realloc(paths, (i + 1) * sizeof(char *));
-    path = strtok(NULL, ":");
-  }
-  paths[i] = NULL;
-  return paths;
+    int size = 2;
+    char **paths = NULL;
+    paths = malloc(size * sizeof(char *));
+    if (paths == NULL)
+        return NULL;
+    int i = 0;
+    char *path;
+    char *c_path;
+    c_path = strdup(str);
+    path = strtok(c_path, ":");
+    while (path != NULL) {
+        if (i == size - 1) {
+            size *= 2;
+            paths = realloc(paths, size * sizeof(char *));
+            
+            if (paths == NULL)
+                return NULL;
+        }
+
+        paths[i] = strdup(path);
+        if (paths[i] == NULL)
+            return NULL;
+        i++;
+        path = strtok(NULL, ":");
+    }
+    paths[i] = NULL;
+    return paths;
 }
 
 char *_strcat(char *dest, char *src)
 {
-  char *hold;
-	int i;
-	int j;
+    char *hold;
+    int i = 0;
+    int j = 0;
 
-	i = 0;
-	j = 0;
-  hold = malloc(sizeof(char *) * (strlen(dest) + strlen(src) + 2));
-	while (dest[i])
-  {
-    hold[i] = dest[i];
-		i++;
-  }
-  hold[i] = '/';
-  i++;
-  while (src[j])
-	{
-		hold[i] = src[j];
-		i++;
-		j++;
-	}
-	hold[i + 1] = '\0';
-  printf("hold >> %s\n",hold);
-	return (hold);
-}
-
-char  *check_path(char **paths, char **input)
-{
-  int i;
-  char *path_cmd;
-
-  i = 0;
-  while (paths[i])
-  {
-    path_cmd = _strcat(paths[i], input[0]);
+    hold = malloc(sizeof(char) * (strlen(dest) + strlen(src) + 2));
+    if (hold == NULL)
+        return NULL;
+    while (dest[i]) {
+        hold[i] = dest[i];
+        i++;
+    }
+    hold[i] = '/';
     i++;
-  }
-  return (NULL);
+    while (src[j]) {
+        hold[i] = src[j];
+        i++;
+        j++;
+    }
+    hold[i] = '\0';
+    return hold;
 }
-void  execution(char **input)
+
+char *check_path(char **paths, char *input)
 {
-  pid_t pid ;
-  int  status;
-  char **paths;
-  char *g_path;
-  int i;
-  char *test;
-  
-  g_path = bring_path("PATH");
-  paths = pathing(g_path);
-  test = check_path(paths, input);
-  printf("%s\n", paths[0]);
+    char *path_cmd = NULL;
+    for (int i = 0; paths[i] != NULL; i++)
+    {
+        path_cmd = _strcat(paths[i], input);   
+        if (access(path_cmd, F_OK | X_OK) == 0) 
+        {
+          return path_cmd;
+        }
+        free(path_cmd);
+    }
+    return NULL;
+}
+
+int execution(char **input, char *test)
+{
+  pid_t pid;
+  int status;
+  char **env = environ;
+
   pid = fork();
-  if (pid == 0)
+  if (pid == -1) 
   {
-    execvp(g_path ,input);
-    printf("done\n");
+    perror("fork");
+    return 0;
   }
-  else if (pid < 0)
+  else if (pid == 0) 
   {
-    printf("error");
+    // Child process
+    execve(test, input, env);
+    free(test);
+    free_buff(input);
   }
-  else
+  else 
     wait(&status);
+  return 1;
 }
 
 int main(int ac, char **av, char **env)
 {
-  char *str = NULL;
+  char *str , *test = NULL;
   size_t buff = 0;
   ssize_t s_read;
-  char **d_str;
+  char **d_str = NULL;
+  char **paths = NULL;
+  char *g_path = NULL;
 
   while (1)
   {
+    free_buff(d_str);
+    free_buff(paths);
+    //free(test);
     prompt();
     s_read = getline(&str, &buff, stdin);
     if (s_read < 0)
@@ -139,20 +175,26 @@ int main(int ac, char **av, char **env)
     }
     if (str[s_read - 1] == '\n')
       str[s_read - 1] = '\0';
+    g_path = bring_path("PATH");
     d_str = parse(str);
-    printf("d_str[0]: %s\n", d_str[0]);
+    for (int i = 0; d_str[i] != NULL; i++)
+        printf("\nd_str[%d]: %s\n", i, d_str[i]);
+    paths = pathing(g_path);
+    test = check_path(paths, d_str[0]);
     if (strcmp(d_str[0], "exit") == 0)
     {
       free(str);
       free(d_str[0]);
-      free(d_str);
+      //d_str[0] = NULL; // set to NULL after freeing
+      free_buff(d_str);
       break;
     }
-    execution(d_str);
-    for (int i = 0; d_str[i] != NULL; i++)
-      printf("d_str[%d]: %s\n", i, d_str[i]);
-    free(d_str[0]);
-    free(d_str);
+    if (!test)
+      perror("problem");
+    else
+      execution(d_str, test);
   }
+
+  
   return 0;
 }
